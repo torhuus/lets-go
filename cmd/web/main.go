da915f3ba97ba17541575760c20da7648aa0599d
Tor Einar Huus <tor@huus.me>
2025-05-30 15:24:45 +0200

    chapter 4.7 done, 4.8 next multi-record sql queries

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,10 +8,12 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"snippetbox.torhuus.com/internal/models"
 )
 
 type application struct {
 	logger *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -34,6 +36,7 @@ func main() {
 
 	app := &application{
 		logger: logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("starting server", slog.String("addr", ":4000"))

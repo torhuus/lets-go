27c92dbc73630de3bfe2a1e43bab8603d2fe1747
Tor Einar Huus <tor@huus.me>
2025-06-02 13:18:07 +0200

    chapter 6 completed, moving onto forms

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) routes() *http.ServeMux {
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}",app.home)
-	mux.HandleFunc("GET /snippet/view/{id}",app.snippetView)
-	mux.HandleFunc("GET /snippet/create",app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create",app.snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return mux
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	return standard.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module snippetbox.torhuus.com
 
 go 1.24.3
 
-require github.com/go-sql-driver/mysql v1.9.2
+require (
+	github.com/go-sql-driver/mysql v1.9.2
+	github.com/justinas/alice v1.2.0
+)
 
 require filippo.io/edwards25519 v1.1.0 // indirect
